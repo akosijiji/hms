f7fcaafa67684efc3ae777b0a70f6471db075c33
akosijiji <say.chiz13@gmail.com>
2013-10-15 20:25:08 +0800

    -- Added some comments

--- logout.php
+++ logout.php
@@ -2,6 +2,8 @@
     session_start();
     session_destroy();
      
+     // Added some comments
+     
     // We redirect them to the login page 
     header("Location: index.php"); 
     die("Redirecting to: index.php");
